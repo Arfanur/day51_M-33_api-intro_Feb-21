c094eed65e90e6d8aacac24b121ddf6c4b1be396
Arfanur <arfanur03@gmail.com>
2022-06-08 13:07:51 +0600

    33-6 Load posts...

--- js/5_practice.js
+++ js/5_practice.js
@@ -1,11 +1,12 @@
-function userInfo() {
+function loadUsers() {
     fetch('https://jsonplaceholder.typicode.com/users')
         .then(res => res.json())
-        .then(data => allUser(data));
+        .then(data => displayUsers(data));
 }
-function allUser(data) {
+function displayUsers(data) {
     const ul = document.getElementById('users');
     for (user of data) {
+        console.log(user);
         const li = document.createElement('li');
         li.innerText = `Name: ${user.name} Email: ${user.email}`;
         ul.appendChild(li);
